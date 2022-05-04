01bd16dbff3a7d1936b41c176e6c12313fcb9153
deuscx <1475453714@qq.com>
2022-05-04 10:54:18 +0800

    feat: vue electron

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        // options placed here will be merged with default configuration and passed to electron-builder
+        win: {
+          artifactName: "Hello Setup ${version}-${arch}.exe",
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64", "ia32"],
+            },
+          ],
+        },
+        nsis: {
+          shortcutName: "Hello",
+          // oneClick: false,
+          // allowToChangeInstallationDirectory: true,
+        },
+      },
+    },
+  },
+});
